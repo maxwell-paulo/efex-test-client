fda45f925c80ea16f02be48080bf5dc422ab8fb6
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-14 18:44:02 -0300

    feat: edit task component

--- src/components/EditTask.jsx
+++ src/components/EditTask.jsx
@@ -1,25 +1,42 @@
 import { useState } from "react";
 import { api } from "../api/api";
-import { connect } from "react-redux";
+import { useSelector } from "react-redux";
+
+import {
+  ModalBackground,
+  ModalContent,
+  ModalField,
+  YesButton,
+  NoButton,
+} from "./StyledComponents";
 
 function EditTask({ taskId, onClose }) {
-  const [editedName, setEditedName] = useState("");
-  const [editedDescription, setEditedDescription] = useState("");
+  const token = useSelector((state) => state.auth.token);
+  const taskDetails = useSelector((state) => state.tasks.taskDetails);
+  const initialName = taskDetails[taskId]?.name || "";
+  const initialDescription = taskDetails[taskId]?.description || "daily task";
+
+  const [editedName, setEditedName] = useState(initialName);
+  const [editedDescription, setEditedDescription] =
+    useState(initialDescription);
 
   const handleEdit = async () => {
+    if (editedName.trim() === "") {
+      alert("O campo de nome não pode ser vazio.");
+      return;
+    }
+
+    const taskUpdate = {
+      name: editedName,
+      description: editedDescription || "daily task",
+    };
+
     try {
-      await api.patch(
-        `/task/${taskId}`,
-        {
-          name: editedName,
-          description: editedDescription,
+      await api.patch(`/task/${taskId}`, taskUpdate, {
+        headers: {
+          Authorization: `Bearer ${token}`,
         },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
+      });
 
       onClose();
     } catch (error) {
@@ -28,29 +45,36 @@ function EditTask({ taskId, onClose }) {
   };
 
   return (
-    <div>
-      <h2>Editar Tarefa</h2>
-      <label htmlFor="editedName">Nome:</label>
-      <input
-        type="text"
-        id="editedName"
-        value={editedName}
-        onChange={(e) => setEditedName(e.target.value)}
-      />
-      <label htmlFor="editedDescription">Descrição:</label>
-      <textarea
-        id="editedDescription"
-        value={editedDescription}
-        onChange={(e) => setEditedDescription(e.target.value)}
-      />
-      <button onClick={handleEdit}>Salvar Edições</button>
-      <button onClick={onClose}>Fechar</button>
-    </div>
+    <ModalBackground>
+      <ModalContent>
+        <ModalField>
+          <h2>EDIT TASK</h2>
+        </ModalField>
+        <ModalField>
+          <label htmlFor="editedName">Name:</label>
+          <input
+            type="text"
+            id="editedName"
+            value={editedName}
+            onChange={(e) => setEditedName(e.target.value)}
+          />
+        </ModalField>
+        <ModalField>
+          <label htmlFor="editedDescription">Description:</label>
+          <input
+            type="text"
+            id="editedDescription"
+            value={editedDescription}
+            onChange={(e) => setEditedDescription(e.target.value)}
+          />
+        </ModalField>
+        <ModalField>
+          <YesButton onClick={handleEdit}>SAVE</YesButton>
+          <NoButton onClick={onClose}>CLOSE</NoButton>
+        </ModalField>
+      </ModalContent>
+    </ModalBackground>
   );
 }
 
-const mapStateToProps = (state) => ({
-  token: state.auth.token,
-});
-
-export default connect(mapStateToProps)(EditTask);
+export default EditTask;
