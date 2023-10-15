4ff063288552a73156316739f3f6b96e3763b619
Maxwell da Silva <maxpaulo.mps@gmail.com>
2023-10-15 00:05:35 -0300

    feat: includes production url

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiURLs = {
   development: "http://localhost:3000/",
-  production: "",
+  production: "https://todolistefex.vercel.app/",
 };
 
 const api = axios.create({ baseURL: apiURLs[process.env.NODE_ENV] });
